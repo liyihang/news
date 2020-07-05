7324fddcc50cfc03524a096825c83c2968d285ac
liyihang <lidoudoou@gmail.com>
2020-07-06 00:02:36 +0800

    home category cache

--- Node-MiddleWare/middleware.js
+++ Node-MiddleWare/middleware.js
@@ -4,10 +4,19 @@ const categoryModel = require('./models/category')
 // 自定义中间件
 exports.golbal = (req, res, next) => {
     res.locals.site = config.site
-    // 分类数据
-    categoryModel.getCategoryTree().then(data => {
-        // 数据的缓存
-        res.locals.category = data
+    // 数据的缓存
+    if (!req.app.locals.category) {
+        // 分类数据
+        categoryModel.getCategoryTree().then(data => {
+
+            res.locals.category = data
+            // 分类没有缓存 就缓存数据
+            res.app.locals.category = data
+            next()
+        }).catch(err => next(err))
+    }else{
+        res.locals.category = res.app.locals.category
         next()
-    }).catch(err => next(err))
+    }
+
 }
